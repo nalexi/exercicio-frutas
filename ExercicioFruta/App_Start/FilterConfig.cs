95a12d9ecbc7c85b64346d249af80ed71df1009b
Nicholas Alexi <nlas.alexi@gmail.com>
2019-01-21 16:09:33 -0200

    Adicionar arquivos de projeto.

---
+++ ExercicioFruta/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ExercicioFruta
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
